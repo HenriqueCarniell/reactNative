40c131faf6c4c3118aff5b31748cf43a21ea4718
Gustavo Henrique <gustavocarniel718@gmail.com>
2024-07-05 16:57:20 -0300

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 function HomeScreen() {
   return (
-    <View>
-      <Text style={{
-        flex: 1,
-        backgroundColor: 'black'
-      }}>React</Text>
-      <Text>Native</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    padding: 24,
+  },
+  eventName: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginTop: 48,
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16
+  }
+})
 
 export default HomeScreen
